9aaffa4f03cf60940314fcdde4854bb019135452
Aaron Klinker <aaronklinker1@gmail.com>
2022-05-30 19:19:35 -0500

    chore: pull api utils out of chrome web store

---
+++ src/apis/cws-api.ts
@@ -0,0 +1,105 @@
+import { checkStatusCode, responseBody } from '../utils/fetch';
+import fetch from 'node-fetch';
+import FormData from 'form-data';
+import fs from 'fs';
+import path from 'path';
+
+export interface CwsApiOptions {
+  clientId: string;
+  clientSecret: string;
+  refreshToken: string;
+}
+
+export interface CwsTokenDetails {
+  access_token: string;
+  expires_in: number;
+  refresh_token: string;
+  scope: string;
+  token_type: string;
+}
+
+export class CwsApi {
+  token?: CwsTokenDetails;
+
+  constructor(readonly options: CwsApiOptions) {}
+
+  private tokenEndpoint() {
+    return new URL('https://oauth2.googleapis.com/token');
+  }
+
+  private uploadEndpoint(extensionId: string) {
+    return new URL(
+      `https://www.googleapis.com/upload/chromewebstore/v1.1/items/${extensionId}`,
+    );
+  }
+
+  private publishEndpoint(extensionId: string) {
+    return new URL(
+      `https://www.googleapis.com/chromewebstore/v1.1/items/${extensionId}/publish`,
+    );
+  }
+
+  async uploadZip(params: { extensionId: string; zipFile: string }) {
+    const Authorization = await this.getAuthHeader();
+
+    console.log('Uploading new ZIP file...');
+    const endpoint = this.uploadEndpoint(params.extensionId);
+    const form = new FormData();
+    form.append(
+      'image',
+      fs.createReadStream(params.zipFile),
+      path.basename(params.zipFile),
+    );
+    await fetch(endpoint.href, {
+      method: 'PUT',
+      body: form,
+      headers: form.getHeaders({
+        Authorization,
+        'x-goog-api-version': 2,
+      }),
+    }).then(checkStatusCode);
+  }
+
+  async submitForReview(params: {
+    extensionId: string;
+    publishTarget?: 'default' | 'trustedTesters';
+  }) {
+    const Authorization = await this.getAuthHeader();
+
+    console.log('Submitting for review...');
+    const endpoint = this.publishEndpoint(params.extensionId);
+    if (params.publishTarget)
+      endpoint.searchParams.append('publishTarget', params.publishTarget);
+
+    await fetch(endpoint.href, {
+      headers: {
+        Authorization,
+        'x-goog-api-version': '2',
+        'Content-Length': '0',
+      },
+    }).then(checkStatusCode);
+  }
+
+  private getToken(): Promise<CwsTokenDetails> {
+    console.log('Getting an access token...');
+    const endpoint = this.tokenEndpoint();
+
+    return fetch(endpoint.href, {
+      method: 'POST',
+      body: JSON.stringify({
+        client_id: this.options.clientId,
+        client_secret: this.options.clientSecret,
+        refresh_token: this.options.refreshToken,
+        grant_type: 'refresh_token',
+        redirect_uri: 'urn:ietf:wg:oauth:2.0:oob',
+      }),
+    })
+      .then(checkStatusCode)
+      .then(responseBody<CwsTokenDetails>());
+  }
+
+  private async getAuthHeader() {
+    this.token ??= await this.getToken();
+    return `${this.token.token_type} ${this.token.access_token}`;
+  }
+}
--- src/stores/chrome-web-store.ts
+++ src/stores/chrome-web-store.ts
@@ -1,9 +1,5 @@
 import { Log } from '../utils/log';
-import fetch, { Response } from 'node-fetch';
-import FormData from 'form-data';
-import fs from 'fs';
-import path from 'path';
-import { checkStatusCode, responseBody } from '../utils/fetch';
+import { CwsApi } from '../apis/cws-api';
 
 export interface ChromeWebStoreOptions {
   zip: string;
@@ -11,15 +7,7 @@ export interface ChromeWebStoreOptions {
   clientId: string;
   clientSecret: string;
   refreshToken: string;
-  publishTarget: 'default' | 'trustedTesters';
-}
-
-interface GcpTokenDetails {
-  access_token: string;
-  expires_in: number;
-  refresh_token: string;
-  scope: string;
-  token_type: string;
+  publishTarget?: 'default' | 'trustedTesters';
 }
 
 export class ChromeWebStore {
@@ -31,71 +19,14 @@ export class ChromeWebStore {
   ) {}
 
   async publish(): Promise<void> {
-    const token = await this.getToken();
-    await this.uploadZip(token);
-    await this.submitForReview(token);
-  }
-
-  getToken(): Promise<GcpTokenDetails> {
-    console.log('Getting an access token...');
-    return fetch(this.tokenEndpoint, {
-      method: 'POST',
-      body: JSON.stringify({
-        client_id: this.options.clientId,
-        client_secret: this.options.clientSecret,
-        refresh_token: this.options.refreshToken,
-        grant_type: 'refresh_token',
-        redirect_uri: 'urn:ietf:wg:oauth:2.0:oob',
-      }),
-    })
-      .then(checkStatusCode)
-      .then(responseBody<GcpTokenDetails>());
-  }
-
-  async uploadZip(token: GcpTokenDetails) {
-    console.log('Uploading new ZIP file...');
-    const form = new FormData();
-    form.append(
-      'image',
-      fs.createReadStream(this.options.zip),
-      path.basename(this.options.zip),
-    );
-    await fetch(this.uploadEndpoint, {
-      method: 'PUT',
-      body: form,
-      headers: form.getHeaders({
-        Authorization: this.getAuthorizationHeader(token),
-        'x-goog-api-version': 2,
-      }),
-    }).then(checkStatusCode);
-  }
-
-  async submitForReview(token: GcpTokenDetails) {
-    console.log('Submitting for review...');
-    await fetch(this.publishEndpoint, {
-      headers: {
-        Authorization: this.getAuthorizationHeader(token),
-        'x-goog-api-version': '2',
-        'Content-Length': '0',
-      },
-    }).then(checkStatusCode);
-  }
-
-  private getAuthorizationHeader(token: GcpTokenDetails): string {
-    return `${token.token_type} ${token.access_token}`;
-  }
-
-  private get tokenEndpoint(): string {
-    return 'https://oauth2.googleapis.com/token';
-  }
-  private get uploadEndpoint(): string {
-    return `https://www.googleapis.com/upload/chromewebstore/v1.1/items/${this.options.extensionId}`;
-  }
-  private get publishEndpoint(): string {
-    if (!['default', 'trustedTesters'].includes(this.options.publishTarget))
-      throw Error(
-        "Chrome's publish target can only be 'default' or 'trustedTesters'",
-      );
-    return `https://www.googleapis.com/chromewebstore/v1.1/items/${this.options.extensionId}/publish?publishTarget=${this.options.publishTarget}`;
+    const api = new CwsApi(this.options);
+    await api.uploadZip({
+      extensionId: this.options.extensionId,
+      zipFile: this.options.zip,
+    });
+    api.submitForReview({
+      extensionId: this.options.extensionId,
+      publishTarget: this.options.publishTarget,
+    });
   }
 }
