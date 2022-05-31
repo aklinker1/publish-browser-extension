b31d55910330bea33dd9e0c65691bcd31208f771
Aaron Klinker <aaronklinker1@gmail.com>
2022-05-30 21:12:26 -0500

    chore: don't use ESModules so node can execute the CLI

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -5,18 +5,18 @@ gulp buildDev
 TARGET="$1"
 
 if [[ "$TARGET" == "all" ]]; then
-    esno dist/cli.js \
+    node dist/cli.js \
         --chrome-zip extension/chrome.zip \
         --firefox-zip extension/firefox.zip \
         --firefox-sources-zip extension/firefox.zip
 elif [[ "$TARGET" == "chrome" ]]; then
-    esno dist/cli.js --chrome-zip extension/chrome.zip
+    node dist/cli.js --chrome-zip extension/chrome.zip
 elif [[ "$TARGET" == "firefox" ]]; then
-    esno dist/cli.js --firefox-zip extension/firefox.zip --firefox-sources-zip extension/firefox.zip
+    node dist/cli.js --firefox-zip extension/firefox.zip --firefox-sources-zip extension/firefox.zip
 elif [[ "$TARGET" == "help" ]]; then
-    esno dist/cli.js --help
+    node dist/cli.js --help
 else
     echo
     echo "Run 'pnpm dev:chrome' or 'pnpm dev:firefox' or 'pnpm dev:all' to run against a specific store"
-    esno dist/cli.js
+    node dist/cli.js
 fi 
