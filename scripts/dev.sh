afbb18353d09e28b35fd0cc9e7d924d236748cb3
Aaron Klinker <aklinker1@users.noreply.github.com>
2023-03-16 14:30:25 -0500

    Fix ci

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -5,18 +5,18 @@ gulp buildDev
 TARGET="$1"
 
 if [[ "$TARGET" == "all" ]]; then
-    node dist/cli.js \
+    node dist/cli.cjs \
         --chrome-zip extension/chrome.zip \
         --firefox-zip extension/firefox.zip \
         --firefox-sources-zip extension/firefox.zip
 elif [[ "$TARGET" == "chrome" ]]; then
-    node dist/cli.js --chrome-zip extension/chrome.zip
+    node dist/cli.cjs --chrome-zip extension/chrome.zip
 elif [[ "$TARGET" == "firefox" ]]; then
-    node dist/cli.js --firefox-zip extension/firefox.zip --firefox-sources-zip extension/firefox.zip
+    node dist/cli.cjs --firefox-zip extension/firefox.zip --firefox-sources-zip extension/firefox.zip
 elif [[ "$TARGET" == "help" ]]; then
-    node dist/cli.js --help
+    node dist/cli.cjs --help
 else
     echo
     echo "Run 'pnpm dev:chrome' or 'pnpm dev:firefox' or 'pnpm dev:all' to run against a specific store"
-    node dist/cli.js
+    node dist/cli.cjs
 fi 
