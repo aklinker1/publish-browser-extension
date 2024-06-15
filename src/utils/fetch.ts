77a2fdbd72c4f7684c8b70e89ba7adde37e272b2
uncenter <47499684+uncenter@users.noreply.github.com>
2024-06-15 19:07:20 -0400

    fix: Fetch error typo

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -3,7 +3,7 @@ import { createFetch } from 'ofetch';
 export const fetch = createFetch({
   defaults: {
     onResponseError: ctx => {
-      console.log('Reqeust:', ctx.request);
+      console.log('Request:', ctx.request);
       console.log('Response:', JSON.stringify(ctx.response, null, 2));
     },
   },
