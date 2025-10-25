29ff1c62f0675dde1890432b4bc73e1324f9e1b9
Aaron <aaronklinker1@gmail.com>
2025-10-24 23:08:38 -0500

    chore!: Drop CJS support and build package with `tsdown` (#45)

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -5,21 +5,21 @@ bun run build
 TARGET="$1"
 
 if [[ "$TARGET" == "all" ]]; then
-    bun bin/publish-extension.cjs \
+    bun bin/publish-extension.mjs \
         --chrome-zip extension/chrome.zip \
         --firefox-zip extension/firefox.zip \
         --firefox-sources-zip extension/firefox.zip \
         --edge-zip extension/chrome.zip
 elif [[ "$TARGET" == "chrome" ]]; then
-    bun bin/publish-extension.cjs --chrome-zip extension/chrome.zip
+    bun bin/publish-extension.mjs --chrome-zip extension/chrome.zip
 elif [[ "$TARGET" == "firefox" ]]; then
-    bun bin/publish-extension.cjs --firefox-zip extension/firefox.zip --firefox-sources-zip extension/firefox.zip
+    bun bin/publish-extension.mjs --firefox-zip extension/firefox.zip --firefox-sources-zip extension/firefox.zip
 elif [[ "$TARGET" == "edge" ]]; then
-    bun bin/publish-extension.cjs --edge-zip extension/chrome.zip
+    bun bin/publish-extension.mjs --edge-zip extension/chrome.zip
 elif [[ "$TARGET" == "help" ]]; then
-    bun bin/publish-extension.cjs --help
+    bun bin/publish-extension.mjs --help
 else
     echo
     echo "Run 'bun dev:chrome' or 'bun dev:firefox' or 'bun dev:edge' or 'bun dev:all'"
-    bun bin/publish-extension.cjs
+    bun bin/publish-extension.mjs
 fi
