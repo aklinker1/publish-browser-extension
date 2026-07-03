ce175ffa3388674326d12e999ab6bdf95d5ba3a6
Aaron <aaronklinker1@gmail.com>
2026-07-03 12:55:32 -0500

    Cleanup

--- scripts/gen-cli-flags.ts
+++ scripts/gen-cli-flags.ts
@@ -2,15 +2,15 @@ import { kebabCase, camelCase } from 'scule';
 import { replaceGeneratedContent } from './utils/code-gen-utils';
 import { configMetas, getDefaultValue } from './utils/config-meta';
 
-const lines: string[] = ['// prettier-ignore', '{'];
-
-for (const meta of configMetas) {
-  const defaultValue = getDefaultValue(meta.schema);
-  const description = `${meta.note ? `[${meta.note}] ` : ''}${meta.description}${defaultValue != null ? ` (default: ${JSON.stringify(defaultValue)})` : ''}`;
-  lines.push(
-    `  cli.option('--${kebabCase(meta.path)} [${camelCase(meta.path)}]', ${JSON.stringify(description)})`,
-  );
-}
-lines.push('}');
+const lines: string[] = [
+  '// prettier-ignore',
+  '{',
+  ...configMetas.map(meta => {
+    const defaultValue = getDefaultValue(meta.schema);
+    const description = `${meta.note ? `[${meta.note}] ` : ''}${meta.description}${defaultValue != null ? ` (default: ${JSON.stringify(defaultValue)})` : ''}`;
+    return `  cli.option('--${kebabCase(meta.path)} [${camelCase(meta.path)}]', ${JSON.stringify(description)})`;
+  }),
+  '}',
+];
 
 await replaceGeneratedContent('src/cli.ts', 'cli-flags', lines.join('\n'));
