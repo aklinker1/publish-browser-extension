4b89de41ac7f1d3b956c3f5ad7c9d52e0821406c
otokotoba <161947343+otokotoba@users.noreply.github.com>
2026-08-28 02:12:31 +0900

    chore: fix test case for resolveConfig

--- src/__tests__/config.test.ts
+++ src/__tests__/config.test.ts
@@ -253,7 +253,7 @@ describe('resolveConfig', () => {
     expect(actual).toEqual(expected);
   });
 
-  it('should resolve config when only fake zip paths are passed', () => {
+  it('should resolve config without a validation error when only fake zip paths are passed', () => {
     const config: InlineConfig = {
       chrome: { zip: '...' },
       firefox: { zip: '...' },
@@ -263,12 +263,7 @@ describe('resolveConfig', () => {
 
     const actual = resolveConfig(config);
 
-    expect(actual).toMatchObject({
-      chrome: { zip: '...' },
-      firefox: { zip: '...' },
-      edge: { zip: '...' },
-      opera: { zip: '...' },
-    });
+    expect(actual).toMatchObject(config);
   });
 });
 
